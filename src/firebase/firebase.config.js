e5ff3d7f312b9e6a7ca4c95e80eb0114c4534226
Razaul007 <raselharuyamare@gmail.com>
2025-01-14 02:31:25 +0600

    project initialized

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5dHBK2o0bKwLWiaJToi_GdCZf7JsoV40",
+  authDomain: "dream-houzing.firebaseapp.com",
+  projectId: "dream-houzing",
+  storageBucket: "dream-houzing.firebasestorage.app",
+  messagingSenderId: "316378739271",
+  appId: "1:316378739271:web:277e3e25fe8083bf8bb762"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
