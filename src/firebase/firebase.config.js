084fa668a28fb30a73bd497093b98d17a22f2dee
Razaul007 <raselharuyamare@gmail.com>
2025-01-14 05:18:37 +0600

    Added SignUp and Login pages

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA5dHBK2o0bKwLWiaJToi_GdCZf7JsoV40",
-  authDomain: "dream-houzing.firebaseapp.com",
-  projectId: "dream-houzing",
-  storageBucket: "dream-houzing.firebasestorage.app",
-  messagingSenderId: "316378739271",
-  appId: "1:316378739271:web:277e3e25fe8083bf8bb762"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,10 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
+import AllProperties from "../pages/AllProperties/AllProperties";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 
 
@@ -7,5 +12,24 @@ import MainLayout from "../layouts/MainLayout";
     {
       path: "/",
       element: <MainLayout/>,
+      errorElement:<ErrorPage/>,
+      children: [
+        {
+            path:"/",
+            element:<Home/>
+        },
+        {
+            path:"all-properties",
+            element: <AllProperties/>
+        },
+        {
+            path:"/login",
+            element:<Login/>
+        },
+        {
+            path:'/signup',
+            element:<SignUp/>
+        }
+      ]
     },
   ]);
